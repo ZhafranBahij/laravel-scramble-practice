df4c0e702b46a121a97884ab3714b0f56880829c
Rainfog <muhammadzhafranbahij@gmail.com>
2024-10-21 20:01:22 +0700

    feat (sanctum): login via sanctum and create, edit, & delete book must use bearer token

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('book', BookController::class);
+Route::apiResource('book', BookController::class)->only(['index', 'show']);
+Route::post('login', [LoginController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('book', BookController::class)->except(['index', 'show']);
+});
